b6d10baf896cd72869f474e5b770ffa89e76f130
unknown <matthewedwardweisfeld@gmail.com>
2020-06-14 23:21:24 -0400

    Updated CORS URL.

--- src/main/java/com/g3productions/charades/Controller.java
+++ src/main/java/com/g3productions/charades/Controller.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "https://remote-charades-backend.herokuapp.com")
 @RestController
 public class Controller {
 
