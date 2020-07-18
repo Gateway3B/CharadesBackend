b729e08f46ef92b347d860c6c2898200f7279345
unknown <matthewedwardweisfeld@gmail.com>
2020-07-17 23:05:09 -0400

    Cross origin enabled for http.

--- src/main/java/com/g3productions/charades/Controller.java
+++ src/main/java/com/g3productions/charades/Controller.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
-@CrossOrigin(origins = "https://remote-charades.herokuapp.com")
+@CrossOrigin(origins = {"https://remote-charades.herokuapp.com", "http://remote-charades.herokuapp.com"})
 @RestController
 public class Controller {
 
